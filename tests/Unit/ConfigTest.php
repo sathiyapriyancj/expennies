bb97976d8434950d10e48240ce9af4eac42b9d7c
Sathiya priyan <ssathiya.cs@gmail.com~>
2024-11-16 14:33:36 +0530

    DataTables & Pagination

--- tests/Unit/ConfigTest.php
+++ tests/Unit/ConfigTest.php
@@ -1,12 +1,11 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace Tests\Unit;
 
 use App\Config;
 use PHPUnit\Framework\TestCase;
-
 
 class ConfigTest extends TestCase
 {
